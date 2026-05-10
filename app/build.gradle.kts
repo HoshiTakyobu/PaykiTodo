759bdbb5c8d65592b468df94cdca95773d288fde
Peiqi Zhao <zopayki@foxmail.com>
2026-05-10 16:42:14 +0800

    1.6.19 缩小自适应图标前景避免桌面裁切

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("com.google.devtools.ksp")
 }
 
-val paykiVersionName = "1.6.18"
-val paykiVersionCode = 90
+val paykiVersionName = "1.6.19"
+val paykiVersionCode = 91
 
 project.setProperty("archivesBaseName", "PaykiTodo-$paykiVersionName")
 
